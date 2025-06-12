5d80dbc82c3a075fd7ead6a9a3acfd06834b0043
maksmuehlbauer <nakeda1987@gmail.com>
2025-06-12 19:48:56 +0200

    add header+nav

--- src/app/main-content/landing/landing.component.ts
+++ src/app/main-content/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from "../../shared/navigation/navigation.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [NavigationComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })

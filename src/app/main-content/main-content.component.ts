03eea1e0c14a5b1058c1bd4ac4e1e5cd8f467ef4
maksmuehlbauer <nakeda1987@gmail.com>
2025-06-12 20:08:21 +0200

    add aboutme comp

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from "./landing/landing.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingComponent],
+  imports: [LandingComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

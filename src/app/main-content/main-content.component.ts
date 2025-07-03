5779803c02b06f8d3d77a5f2c68f0861e222b0c8
maksmuehlbauer <nakeda1987@gmail.com>
2025-07-03 19:40:11 +0200

    add footer

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillSetComponent } from "./skill-set/skill-set.component";
 import { MyWorkComponent } from "./my-work/my-work.component";
 import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent, ContactComponent],
+  imports: [LandingComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent, ContactComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

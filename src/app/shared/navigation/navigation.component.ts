bacd8d5992b9343426b99a3c1321a0fae9cbfa75
maksmuehlbauer <nakeda1987@gmail.com>
2025-07-23 17:52:34 +0200

    add sidemenu and styles

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  menuIsVisible: boolean = false
+
+  toggleMenu() {
+    this.menuIsVisible = !this.menuIsVisible
+    console.log(this.menuIsVisible)
+  }
+
 
 }
